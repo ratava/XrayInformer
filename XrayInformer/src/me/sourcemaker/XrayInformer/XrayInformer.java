c225d86faf231b70a0f0fe16e8ef7fecc1e3dc6f
Ben <b.bolzmann@live.de>
2012-02-17 07:54:14 +0100

    Inserted license (CC BY-NC-ND 3.0)

--- XrayInformer/src/me/sourcemaker/XrayInformer/XrayInformer.java
+++ XrayInformer/src/me/sourcemaker/XrayInformer/XrayInformer.java
@@ -1,3 +1,17 @@
+/**
+ * XrayInformer - A warning solution especially against xray-using players
+ *
+ * The XRI informs about farmrates and their possibilities. It's useful for every serveradmin
+ *
+ * @package    me.sourcemaker.XrayInformer
+ * @author     sourcemaker
+ * @copyright  widecraft.de
+ * @license    Attribution-NonCommercial-NoDerivs 3.0 Unported (CC BY-NC-ND 3.0) (http://creativecommons.org/licenses/by-nc-nd/3.0/)
+ * @version    Release: 2.1.6
+ * @link       http://dev.bukkit.org/server-mods/xray-informer
+ *
+ */
+
 package me.sourcemaker.XrayInformer;
 
 import java.io.BufferedReader;
